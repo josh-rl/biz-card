6ba8a9032e37e4c39ac06a6a04d1aa3b9583e7f9
Joshua Lawrinenko <jrlawrinenko@gmail.com>
2023-06-23 13:58:33 -0500

    added atlas launch config

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -4,20 +4,25 @@ class Connection {
 	constructor() {
 		let url = "";
 		if (process.env.NODE_ENV === "production") {
-			url = process.env.MONGODB_PROD_URI;
+			url = process.env.MONGODB_ATLAS_URI;
 		}
 		else if (process.env.NODE_ENV === "development") {
-			url = process.env.MONGODB_DEV_URI;
+			if (process.env.MONGO_MODE === "local") {
+				url = process.env.MONGODB_DEV_URI;
+			}
+			else if (process.env.MONGO_MODE === "atlas") {
+				url = process.env.MONGODB_ATLAS_URI;
+			}
 		}
-		// if (url !== "") {
-		// 	url = (url.replace(
-		// 		"<username>",
-		// 		process.env.DB_USER
-		// 	)).replace(
-		// 		"<password>",
-		// 		process.env.DB_PASS
-		// 	);
-		// }
+		if (url !== "") {
+			url = (url.replace(
+				"<username>",
+				process.env.DB_USER
+			)).replace(
+				"<password>",
+				process.env.DB_PASS
+			);
+		}
 		mongoose.Promise = global.Promise;
 		mongoose.connect(url, {
 			dbName: process.env.DB_NAME,
